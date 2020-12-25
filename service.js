eafa649fdcdf854226e5929b82e906c31a18c6d6
alexmarucci <33167212+alexmarucci@users.noreply.github.com>
2020-12-25 17:37:55 +0000

    Create service.js

---
+++ service.js
@@ -0,0 +1,31 @@
+import { createResourceState, createSignal, createState } from "solid-js";
+
+const loadableDeps = new Map();
+const deps = new Map();
+
+function loadService(id, args) {
+  const l = loadableDeps.get(id);
+
+  if (l) {
+    deps.set(id, l(...args));
+    loadableDeps.delete(id);
+  }
+
+  return deps.get(id);
+}
+
+export function createService(fn) {
+  const callable = typeof fn === "function";
+  const id = Symbol(fn);
+
+  if (callable) loadableDeps.set(id, fn);
+  else loadableDeps.set(id, () => fn);
+
+  return id;
+}
+
+export function useService(id, ...args) {
+  const d = deps.get(id);
+
+  return d ?? loadService(id, args);
+}
